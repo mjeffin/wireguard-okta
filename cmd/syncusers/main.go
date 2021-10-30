c9b25a2c89bb5a414375a4e9dc9272aba5236764
jma <jeffin@jusfocus.com>
2021-10-30 15:49:28 +0530

    getting the list of allowed emails

--- cmd/syncusers/main.go
+++ cmd/syncusers/main.go
@@ -9,20 +9,28 @@ import (
 
 func main()  {
 	conf.LoadEnvFile()
-	_,wgNet,_ := internal.GetWgCIDR()
-	if err := internal.CreateDBSchema(); err != nil {
-		log.Println("Error creating db schema ", err)
-	}
-	usedIPs := internal.GetUsedIPs()
-	nextIp, err := internal.GetNextIp(wgNet,usedIPs)
-	if err != nil {
-		fmt.Println("Error fetching next ip ",err)
-	}
-	fmt.Println(nextIp)
+	//_,wgNet,_ := internal.GetWgCIDR()
+	//if err := internal.CreateDBSchema(); err != nil {
+	//	log.Println("Error creating db schema ", err)
+	//}
+	//usedIPs := internal.GetUsedIPs()
+	//nextIp, err := internal.GetNextIp(wgNet,usedIPs)
+	//if err != nil {
+	//	fmt.Println("Error fetching next ip ",err)
+	//}
+	//fmt.Println(nextIp)
 
-	//config, _ := conf.GetOktaServerConfig()
-	//oh := internal.OktaHandler{Conf: config}
-	//oh.GetUsers()
+	config, err := conf.GetOktaServerConfig()
+	if err != nil {
+		log.Println("Error fetching okta config")
+	}
+	oh := internal.OktaHandler{Conf: config}
+	userEmails,err := oh.GetAllowedEmails()
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(userEmails)
+
 }
 
 
--- internal/okta.go
+++ internal/okta.go
@@ -1,10 +1,9 @@
 package internal
 
-
 import (
 	"context"
 	"encoding/json"
-	"fmt"
+	"errors"
 	"github.com/okta/okta-sdk-golang/v2/okta"
 	"log"
 	"mjeffn/wireguard-okta/pkg/conf"
@@ -15,24 +14,44 @@ type OktaHandler struct {
 	Conf conf.OktaServerConfig
 }
 
-type UserProfile struct {
+type userProfile struct {
 	Email string `json:"email"`
 }
 
-func (oh OktaHandler) GetUsers()  {
+// GetAllowedEmails calls Okta API and returns the list of email addresses for the specified group
+// TODO - check about okta pagination and the default number of users in one response.
+// uses json marshaling and unmarshalling to extract the emails from user profiles which is map[string]interface{}.
+// not sure if it's the best way, but it gets the job done.
+func (oh OktaHandler) GetAllowedEmails() ([]string,error) {
 	ctx, client, err := okta.NewClient(context.Background(),
 		okta.WithOrgUrl(oh.Conf.OrgUrl), okta.WithToken(oh.Conf.ApiToken))
 	if err != nil {
-		log.Println("error initiating client",err)
+		log.Println(err)
+		return nil, errors.New("error initiating okta handler")
 	}
 	users, _, err := client.Group.ListGroupUsers(ctx,oh.Conf.WireguardGroupId,nil)
 	if err != nil {
-		log.Println("Error fetching users of group")
+		log.Println( err)
+		return nil,errors.New("error fetching users of given group from okta")
 	}
-	fmt.Println(users[0].Profile)
-	var u UserProfile
-	b,_ := json.Marshal(users[0].Profile)
-	json.Unmarshal(b,&u)
-	fmt.Println(u)
-
+	var oupl []*okta.UserProfile
+	for _,u := range users {
+		oupl = append(oupl,u.Profile)
+	}
+	b, err := json.Marshal(oupl)
+	if err != nil {
+		log.Println(err)
+		return nil,errors.New("error marshaling user profiles")
+	}
+	var ups []userProfile
+	err = json.Unmarshal(b,&ups)
+	if err != nil {
+		log.Println(err)
+		return nil,errors.New("error extracting emails from okta users")
+	}
+	var ues []string
+	for _, up := range ups {
+		ues = append(ues,up.Email)
+	}
+	return ues,nil
 }
